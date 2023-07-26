fcd7b31dee1fe332a6afb34a9c7a7ae63438b02c
QK391 <90550481+QK391@users.noreply.github.com>
2023-07-26 23:09:58 +0700

    build UI header 100

--- tiktok-ui/src/components/Layout/components/Header/index.js
+++ tiktok-ui/src/components/Layout/components/Header/index.js
@@ -1,65 +1,94 @@
-import classNames from 'classnames/bind';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
-import Tippy from '@tippyjs/react/headless';
-import styles from './Header.module.scss';
-import images from '~/assets/images';
-import { useEffect, useState } from 'react';
-import { Wrapper as PopperWrapper } from '~/components/Popper';
-import Accountitem from '../../../Accountitem';
-import Button from '~/components/Button';
+import classNames from "classnames/bind";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleQuestion,
+  faCircleXmark,
+  faEarthAsia,
+  faEllipsisVertical,
+  faKeyboard,
+  faMagnifyingGlass,
+  faSpinner,
+} from "@fortawesome/free-solid-svg-icons";
+import Tippy from "@tippyjs/react/headless";
+import styles from "./Header.module.scss";
+import images from "~/assets/images";
+import { useEffect, useState } from "react";
+import { Wrapper as PopperWrapper } from "~/components/Popper";
+import Accountitem from "../../../Accountitem";
+import Button from "~/components/Button";
+import Menu from "~/components/Popper/Menu";
 const cx = classNames.bind(styles);
 
+const MENU_ITEMS = [
+  {
+    icon: <FontAwesomeIcon icon={faEarthAsia} />,
+    title: 'English',
+  },
+  {
+    icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+    title: 'Feedback and Help',
+    to: '/feedback'
+  },
+  {
+    icon: <FontAwesomeIcon icon={faKeyboard} />,
+    title: 'Keyboard shortcuts',
+  },
+];
 function Header() {
-    const [searchResunlt, setSearchResunlt] = useState([]);
+  const [searchResunlt, setSearchResunlt] = useState([]);
 
-    useEffect(() => {
-        setTimeout(() => {
-            setSearchResunlt([]);
-        }, 0);
-    }, [])
-    return <header className={cx('wrapper')}>
-        <div className={cx('inner')}>
-            <img src={images.logo} alt="Tiktok" />
+  useEffect(() => {
+    setTimeout(() => {
+      setSearchResunlt([]);
+    }, 0);
+  }, []);
+  return (
+    <header className={cx("wrapper")}>
+      <div className={cx("inner")}>
+        <img src={images.logo} alt="Tiktok" />
 
-            <Tippy interactive
-            visible= {searchResunlt.length > 0} 
-            render={(attrs) => (
-               
-                <div className={cx('search-result')} tabIndex="-1"{...attrs}>
-                   <PopperWrapper>
-                    <h4 className={cx('search-title')}>
-                        Accounts
-                    </h4>
-                    <Accountitem/>
-                    <Accountitem/>
-                    <Accountitem/>
-                    <Accountitem/>
-                    <Accountitem/>
-                    </PopperWrapper>
-                </div>
-               
-            )}>
-                <div className={cx('search')}>
-                    <input placeholder='Search accounts and videos' />
-
-                    <button className={cx('clear')}>
-                        <FontAwesomeIcon icon={faCircleXmark} />
-                    </button>
-
-                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-
-                    <button className={cx('search-btn')}>
-                        <FontAwesomeIcon icon={faMagnifyingGlass} />
-                    </button>
-                </div>
-            </Tippy>
-            <div className={cx('actions')}>
-                <Button text> Upload </Button>
-                <Button primary> Log in </Button>
-
+        <Tippy
+          interactive
+          visible={searchResunlt.length > 0}
+          render={(attrs) => (
+            <div className={cx("search-result")} tabIndex="-1" {...attrs}>
+              <PopperWrapper>
+                <h4 className={cx("search-title")}>Accounts</h4>
+                <Accountitem/>
+                <Accountitem/>
+                <Accountitem/>
+                <Accountitem/>
+                <Accountitem/>
+              </PopperWrapper>
             </div>
+          )}
+        >
+          <div className={cx("search")}>
+            <input placeholder="Search accounts and videos" />
+
+            <button className={cx("clear")}>
+              <FontAwesomeIcon icon={faCircleXmark} />
+            </button>
+
+            <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
+
+            <button className={cx("search-btn")}>
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
+          </div>
+        </Tippy>
+        <div className={cx("actions")}>
+          <Button text> Upload </Button>
+          <Button primary> Log in </Button>
+
+          <Menu items = {MENU_ITEMS}>
+            <button className={cx("more-btn")}>
+              <FontAwesomeIcon icon={faEllipsisVertical} />
+            </button>
+          </Menu>
         </div>
-    </header>;
+      </div>
+    </header>
+  );
 }
 export default Header;
